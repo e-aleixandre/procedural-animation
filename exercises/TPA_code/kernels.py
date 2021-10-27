2bc3d3c83f848cba4f908ec3f62f83a67e410a6f
E.Aleixandre <ealeixandredev@gmail.com>
2021-10-27 20:16:40 +0200

    Finished implementing kernels

--- exercises/TPA_code/kernels.py
+++ exercises/TPA_code/kernels.py
@@ -31,9 +31,12 @@ class StandardKernel:
 
     def value(self,x):
         r2 = normsquared(x)
-        # TODO: complete the kernel!
-        return r2 #TPA
-        #
+
+        if np.sqrt(r2) > self._h:
+            return 0
+        
+        return self._coef_kern * pow(self._h2 - r2, 3)
+            
     # ------
 
 
@@ -41,7 +44,14 @@ class StandardKernel:
 
     def gradient(self,x):
         g = np.zeros((3,1))
-        # TODO: complete the kernel!
+        r2 = normsquared(x)
+
+        if np.sqrt(r2) > self._h:
+            return g
+
+        for i in range(3):
+            g[i][0] = - self._coef_grad * x[i] * pow(self._h2 - r2, 2)
+
         return g #TPA
     # ------
 
@@ -49,8 +59,11 @@ class StandardKernel:
 
     def laplacian(self,x):
         r2 = normsquared(x)
-        # TODO: complete the kernel!
-        return r2 #TPA
+        
+        if np.sqrt(r2) > self._h:
+            return 0
+
+        return - self._coef_lapl * (self._h2 - r2) * (3 * self._h2 - 7 * r2)
         #
     # ------
 
@@ -78,24 +91,39 @@ class SpikyKernel:
 
     def value(self,x):
         r = np.linalg.norm(x)
-        # TODO: complete the kernel!
-        return r #TPA
+        
+        if r > self._h:
+            return 0
+
+        return self._coef_kern * pow(self._h - r, 3)
         #
     # ------
 
 
     def gradient(self,x):
         g = np.zeros((3,1))
-        # TODO: complete the kernel!
-        return g #TPA
+        r = np.linalg.norm(x)
+        dist_squared = pow(self._h - r, 2)
+
+        if r > self._h or r == 0:
+            return g
+
+        for i in range(3):
+            g[i][0] = - self._coef_grad * x[i] / r * dist_squared
+
+        return g
+            
     # ------
 
 
 
     def laplacian(self,x):
         r = np.linalg.norm(x);
-        # TODO: complete the kernel!
-        return r #TPA
+        
+        if r > self._h or r == 0:
+            return 0
+        
+        return - self._coef_lapl * 1 / r *  (self._h - r) * (self._h - 2 * r)
     # ------
 
 ####  End of class
@@ -125,24 +153,38 @@ class ViscosityKernel:
 
     def value(self,x):
         r = np.linalg.norm(x)
-        # TODO: complete the kernel!
-        return r #TPA
+        
+        if r > self._h or r == 0:
+            return 0
+
+        return self._coef_kern * (- pow(r, 3) / (2 * self._h3) + pow(r, 2) / self._h2 + self._h / (2 * r) - 1)
         #
     # ------
 
 
     def gradient(self,x):
         g = np.zeros((3,1))
-        # TODO: complete the kernel!
+        
+        r = np.linalg.norm(x)
+
+        if r > self._h or r == 0:
+            return g
+
+        for i in range(3):
+            g[i][0] = self._coef_grad * x[i] * (- 3 * r / (2 * self._h3) + 2 / self._h2 - self._h / (2 * pow(r, 3)) )
+
         return g #TPA
     # ------
 
 
 
     def laplacian(self,x):
-        r = np.linalg.norm(x);
-        # TODO: complete the kernel!
-        return r #TPA
+        r = np.linalg.norm(x)
+
+        if r > self._h:
+            return 0
+
+        return self._coef_lapl * (self._h - r) #TPA
     # ------
 
 ####  End of class
