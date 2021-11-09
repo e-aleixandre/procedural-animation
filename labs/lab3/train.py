9187d8c0ce0f74703d79687782a12951483d2520
E.Aleixandre <ealeixandredev@gmail.com>
2021-11-09 20:27:44 +0100

    Adds argument parsing for lab3

--- labs/lab3/train.py
+++ labs/lab3/train.py
@@ -1,15 +1,50 @@
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
-env = gym.make('FrozenLake8x8-v1', is_slippery=False)
+"""
+    Declarations
+"""
+alpha = 0.1
+gamma = 0.95
+environment = "FrozenLake-v0"
+episodes = 5000
+steps = 100
+
+"""
+    Argument definition
+"""
+parser = argparse.ArgumentParser()
+parser.add_argument("--alpha", "-a")
+parser.add_argument("--gamma", "-g")
+parser.add_argument("--episodes", "-e")
+parser.add_argument("--steps", "-s")
+parser.add_argument("environment")
+
+"""
+    Argument parsing
+"""
+args = parser.parse_args()
+
+alpha = alpha if args.alpha is None else float(args.alpha)
+gamma = gamma if args.gamma is None else float(args.gamma)
+environment = environment if args.environment is None else args.environment
+episodes = episodes if args.episodes is None else int(args.episodes)
+steps = steps if args.steps is None else int(args.steps)
+
+"""
+    Initialization
+"""
+env = gym.make(environment, is_slippery=False)
 
 Q = np.zeros([env.observation_space.n, env.action_space.n])
 
-alpha = 0.9
-gamma = 0.9
-episodes = 5000
 rev_list = []
+
+"""
+    Q-Learning
+"""
 
 for i in range(episodes):
     s = env.reset()
@@ -20,22 +55,21 @@ for i in range(episodes):
     while j < 99:
         j += 1
 
-        a = np.argmax(Q[s,:] + np.random.randn(1, env.action_space.n) * (1. / (i + 1)))
-        
+        a = np.argmax(Q[s, :] + np.random.randn(1, env.action_space.n) * (1. / (i + 1)))
+
         s1, r, d, _ = env.step(a)
 
         Q[s, a] = Q[s, a] + alpha * (r + gamma * np.max(Q[s1, :]) - Q[s, a])
         rAll += r
         s = s1
-        if d == True:
+        if d:
             break
 
         rev_list.append(rAll)
-        
+
 np.savetxt("Q_table.txt", Q)
 
 plt.plot(rev_list)
 plt.show()
-
 
 env.close()
